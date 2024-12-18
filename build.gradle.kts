e95f831b44f225d02c5b4559327bcb7889fd5599
Ate Pelsma <87018841+ate-pelsma@users.noreply.github.com>
2024-12-18 12:56:34 +0100

    remove unnecessary spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.3.5"
-    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.keylane"
@@ -19,10 +17,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- src/main/kotlin/advent/AdventApplication.kt
+++ src/main/kotlin/advent/AdventApplication.kt
@@ -1,19 +1,10 @@
-/*
- * Copyright (c) Keylane B.V. 2001 - 2024
- */
-
 package advent
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import java.io.File
 
-@SpringBootApplication
 class AdventApplication
 
 fun main(args: Array<String>) {
-    runApplication<AdventApplication>(*args)
-
     val filePath = "src/main/resources/input.txt"
     val lines = File(filePath).readLines()
 
